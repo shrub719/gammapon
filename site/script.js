0c667be8a1c225292263bb2f8164e93633bf24d8
shrub <shrub719@gmail.com>
2026-03-15 00:37:24 +0000

    add iframe to mathworld

--- site/script.js
+++ site/script.js
@@ -1,11 +1,1 @@
-const result = document.getElementById("result");
 
-const url = "https://mathworld.wolfram.com/cgi-bin/random.cgi";
-
-async function getRandomPage() {
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-}
-
-getRandomPage();
