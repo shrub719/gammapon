a4807ead38bdd92335e56369265b233770587956
shrub <shrub719@gmail.com>
2026-03-17 22:05:24 +0000

    add rudimentary collection page

---
+++ site/collection/script.js
@@ -0,0 +1,14 @@
+const collection = document.getElementById("collection");
+
+let collection_json = JSON.parse(
+    localStorage.getItem("gacha:collection") ?? "[]"
+);
+
+collection_json.forEach((page) => {
+    const title = document.createElement("a");
+    title.innerText = page.title;
+    title.href = `${URL}/${page.id}.html`;
+    title.target = "_blank";
+    collection.appendChild(title);
+});
+
